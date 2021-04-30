b40891962ef1b0de675c0e54e7b78a7ee9fb79c2
Daz DeBoer <daz@gradle.com>
2021-04-30 10:36:07 -0600

    Add sources to Spotbugs example

--- verified-plugins/com.github.spotbugs/spotbugs-example/build.gradle.kts
+++ verified-plugins/com.github.spotbugs/spotbugs-example/build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
+    java
     id("com.github.spotbugs") version "4.6.0"
+}
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Use JUnit test framework.
+    testImplementation("junit:junit:4.13.1")
 }
