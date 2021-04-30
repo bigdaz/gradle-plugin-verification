bee259ba5475dfffd5e77d52a2f5b7d0e93112f0
Daz DeBoer <daz@gradle.com>
2021-04-30 11:53:53 -0600

    Specify default tasks in example builds

--- verified-plugins/com.github.spotbugs/spotbugs-example/build.gradle.kts
+++ verified-plugins/com.github.spotbugs/spotbugs-example/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("com.github.spotbugs") version "4.6.0"
 }
+defaultTasks("clean", "build")
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
