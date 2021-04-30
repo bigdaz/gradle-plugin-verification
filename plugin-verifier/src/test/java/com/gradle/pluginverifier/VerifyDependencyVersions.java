9bbca8ee6a0c7d355c1c78ff1178e6c5609fef98
Daz DeBoer <daz@gradle.com>
2021-04-30 11:54:28 -0600

    Add example for com.github.ben-manes.versions

---
+++ plugin-verifier/src/test/java/com/gradle/pluginverifier/VerifyDependencyVersions.java
@@ -0,0 +1,9 @@
+package com.gradle.pluginverifier;
+
+public class VerifyDependencyVersions extends AbstractVerifyPluginsTest {
+
+    @Override
+    protected String getExampleBuild() {
+        return "com.github.ben-manes.versions/versions-example";
+    }
+}
