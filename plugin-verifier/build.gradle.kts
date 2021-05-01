09db4e979c344ae98d2ee6481a0dd7de49f80444
Daz DeBoer <daz@gradle.com>
2021-05-01 07:34:31 -0600

    Copy samples to build directory for execution

--- plugin-verifier/build.gradle.kts
+++ plugin-verifier/build.gradle.kts
@@ -8,3 +8,13 @@ dependencies {
     testImplementation(gradleTestKit())
     testImplementation("junit:junit:4.13.1")
 }
+
+tasks.register<Copy>("copyPluginProjects") {
+    from("../verified-plugins")
+    into("build/verified-plugins")
+}
+
+tasks.test {
+    dependsOn("copyPluginProjects")
+    useJUnitPlatform()
+}
