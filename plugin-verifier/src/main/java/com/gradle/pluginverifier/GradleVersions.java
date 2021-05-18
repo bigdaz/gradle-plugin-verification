020d348b97d00e26835c92719c5b1ff6608523a7
Daz DeBoer <daz@gradle.com>
2021-05-18 11:49:04 -0600

    Bump to use Gradle 7.0.2

--- plugin-verifier/src/main/java/com/gradle/pluginverifier/GradleVersions.java
+++ plugin-verifier/src/main/java/com/gradle/pluginverifier/GradleVersions.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GradleVersions {
-    private static List<String> versions = Arrays.asList("7.0", "6.8.3", "5.6.4");
+    private static List<String> versions = Arrays.asList("7.0.2", "6.8.3", "5.6.4");
 
     public static String getLatest() {
         return versions.get(0);
