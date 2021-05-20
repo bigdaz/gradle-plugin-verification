4854822a2e89e0e3c98c210065354b4d381400ef
Daz DeBoer <daz@gradle.com>
2021-05-20 12:19:25 -0600

    Add sample for io.spring.nohttp plugin
    This sample demonstrates that our caching test does not test for absolute paths.

---
+++ verified-plugins/io.spring.nohttp/src/main/java/org/example/Library.java
@@ -0,0 +1,9 @@
+/* Licensed under Apache-2.0 */
+package org.example;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        String val = "https://google.com"
+        return true;
+    }
+}
